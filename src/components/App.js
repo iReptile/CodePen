dee9c41048f1db8ea507dda726afe7cd75c488f0
Patrick Singh <info@ireptile.nl>
2021-04-12 08:29:45 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Editor from './Editor';
+
+function App() {
+	return (
+		<>
+			<div className="pane top-pane">
+				<Editor/>
+				<Editor/>
+				<Editor/>
+			</div>
+			<div className="pane"></div>
+			<iframe
+				title="output"
+				sandbox="allow-scripts"
+				frameBorder="0"
+				width="100%"
+				height="100%"
+			/>
+				
+			
+	 	</>
+	);
+}
+
+export default App;
