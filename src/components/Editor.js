126c5b96047035e6d46360f74d6ed62ed2dd53a7
Patrick Singh <info@ireptile.nl>
2021-04-12 08:36:31 +0200

    Updated App.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -29,6 +29,7 @@ export default function Editor(props) {
                     lineWrapping: true,
                     lint: true,
                     mode: language,
+                    theme: "monokai",
                     lineNumbers: true
                 }}
             />
