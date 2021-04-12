9c024c31b4d47560eefc4b71c43fdf9f4e01808d
Patrick Singh <info@ireptile.nl>
2021-04-12 13:15:45 +0200

    Completed the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from './Editor';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-	const [html, setHtml] = useState('');
-	const [css, setCss] = useState('');
-	const [javascript, setJavascript] = useState('');
+	const [html, setHtml] = useLocalStorage('html', '');
+	const [css, setCss] = useLocalStorage('css', '');
+	const [javascript, setJavascript] = useLocalStorage('javascript', '');
+	const [srcDoc, setSrcDoc] = useState('');
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setSrcDoc(`
+				<html>
+					<body>${html}</body>
+					<style>${css}</style>
+					<script>${javascript}</script>
+				</html>
+			`);
+		},250);
+
+		return () => clearTimeout(timeout);
+	}, [html, css, javascript]);
+
 	return (
 		<>
 			<div className="pane top-pane">
@@ -24,16 +41,16 @@ function App() {
 					value={javascript} 
 					onChange={setJavascript}/>
 			</div>
-			<div className="pane"></div>
-			<iframe
-				title="output"
-				sandbox="allow-scripts"
-				frameBorder="0"
-				width="100%"
-				height="100%"
-			/>
-				
-			
+			<div className="pane">
+				<iframe
+					srcDoc={srcDoc}
+					title="output"
+					sandbox="allow-scripts"
+					frameBorder="0"
+					width="100%"
+					height="100%"
+				/>
+			</div>
 	 	</>
 	);
 }
